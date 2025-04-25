26f89d6bb804b20715fd4db9cdfbd1e9605ab40b
AdvitG <garg.advit@gmail.com>
2025-04-25 20:06:20 +0530

    some updates

---
+++ assignment1/bst.cpp
@@ -0,0 +1,36 @@
+/* Write a program for a Binary Search Tree (BST) having functions for insertion, deletion, traversing(using pre order post order in order), maximum and minimum depth.*/
+
+#include <iostream> 
+using namespace std;
+
+class node{
+
+    int data; 
+    node* left;
+    node* right; 
+
+    public: 
+    node* creat_node(int a){
+        node* newnode = new node;
+        newnode->data=a; 
+        newnode->left=nullptr;
+        newnode->right=nullptr;
+        return newnode;
+
+    }
+    node* insert(node* root, int a){
+        if(root==nullptr){
+            
+            return creat_node(a);}
+
+    }
+
+
+
+};
+
+int main(){
+
+    return 0;
+
+} 
